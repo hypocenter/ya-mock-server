e04f91ed14e2ae34adaa4d5cfa616e4a8bfed2e9
hypo <hypomail@gmail.com>
2018-05-07 09:40:26 +0800

    修改 Result.data 为 Result.payload

--- example/mock.js
+++ example/mock.js
@@ -1,5 +1,5 @@
-const path = require('path')
+const path = require('path');
 const { MockServer } = require('../index');
 
-const mockServer = new MockServer('http://127.0.0.1:3000', path.join(__dirname, 'data', '*.mock.js'))
-mockServer.start()
+const mockServer = new MockServer('http://127.0.0.1:3000', path.join(__dirname, 'data', '*.mock.js'));
+mockServer.start();
